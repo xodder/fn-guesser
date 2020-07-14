965c210ef53955c57c304c85960d13e21a988e9b
Stephen Odebiyi <odebiyistephen@gmail.com>
2020-07-15 00:24:22 +0100

    implementing function_guesser with the frame sliding technique

--- function_guesser/function_guesser.cpp
+++ function_guesser/function_guesser.cpp
@@ -8,13 +8,18 @@ namespace xod
     std::string guess_function(std::vector<int> inputs, std::vector<int> outputs)
     {
         const int input_count = inputs.size();
-        const double precision = 0.00001;
+        const double max_error = 1e-10;
 
-        for (int frame_size = 2; frame_size <= input_count; frame_size++)
+        for (int frame_size = 1; frame_size <= input_count; frame_size++)
         {
             int row_count = frame_size;
             int col_count = frame_size + 1;
             double matrix[row_count * col_count];
+
+            // (4, 2) -> [0,1], [0,2], [0,3], [1, 2], [1, 3], [2, 3]
+            // for (int i = 0; i < input_count; ++i)
+            // {
+            // }
 
             // frame sliding
             // frame_size = 2
@@ -28,12 +33,12 @@ namespace xod
             int page_start_index = 0;
             int page_end_index = page_start_index + frame_size;
             bool skip_current_frame = false;
-            std::vector<double> prev_solution;
-
-            std::cout << page_end_index << ", " << input_count - frame_size << std::endl;
+            double smallest_error;
+            std::vector<double> best_solution;
+            bool is_first_test = true;
 
             // frame sliding implementation
-            while (!skip_current_frame && page_end_index <= input_count)
+            while (!skip_current_frame && page_end_index < input_count)
             {
                 // build simultaneous equations for page
                 for (int i = page_start_index; i < page_end_index; ++i)
@@ -41,36 +46,63 @@ namespace xod
                     // transform i into 0...n range
                     const int zeroed_i = i - page_start_index;
                     const int offset = zeroed_i * col_count;
+
                     // build augmented matrix for input and output set at i
-                    if (frame_size == 2)
+                    for (int j = 0; j < frame_size; ++j)
                     {
-                        matrix[offset + 0] = inputs[i];
-                        matrix[offset + 1] = 1;
-                        matrix[offset + 2] = outputs[i];
+                        matrix[offset + j] = std::pow(inputs[i], frame_size - j - 1);
                     }
-                    else if (frame_size == 3)
-                    {
-                        matrix[offset + 0] = std::pow(inputs[i], 2);
-                        matrix[offset + 1] = i + 1;
-                        matrix[offset + 2] = 1;
-                        matrix[offset + 3] = outputs[i];
-                    }
-                    else if (frame_size == 4)
-                    {
-                        matrix[offset + 0] = std::pow(inputs[i], 3);
-                        matrix[offset + 1] = std::pow(inputs[i], 2);
-                        matrix[offset + 2] = zeroed_i + 1;
-                        matrix[offset + 3] = 1;
-                        matrix[offset + 4] = outputs[i];
-                    }
+                    matrix[offset + frame_size] = outputs[i]; // augment with output
                 }
 
                 auto solution = xod::guass_jordan_elimination(matrix, row_count, col_count);
 
-                // compare current solution and previous one
-                if (page_start_index > 0)
+                // compare the output of the function from the current solution
+                // to the actual output given for each input
+                double biggest_error = 0;
+
+                for (int i = 0; i < input_count; ++i)
                 {
-                    for (int i = 0; i < frame_size; i++)
+                    const double input = inputs[i];
+                    const double actual_output = outputs[i];
+                    double output_from_solution = 0;
+
+                    for (int j = 0; j < row_count; ++j)
+                    {
+                        output_from_solution += (solution[i] * std::pow(input, j)); // a_i * x^i
+                    }
+
+                    double error = std::abs(output_from_solution - actual_output);
+
+                    if (error > biggest_error)
+                    {
+                        biggest_error = error;
+                    }
+                }
+
+                if (biggest_error < smallest_error || is_first_test)
+                {
+                    best_solution = solution;
+                    smallest_error = biggest_error;
+                }
+
+                is_first_test = false;
+
+                // std::cout << page_start_index << ", " << page_end_index << std::endl;
+
+                std::cout << "solution = [";
+                for (int i = 0; i < row_count; ++i)
+                {
+                    std::cout << best_solution[i] << ((i + 1) % row_count ? "," : "");
+                }
+                std::cout << "]" << std::endl;
+
+                if (page_end_index == input_count - 1 && smallest_error < max_error)
+                {
+                    return "";
+                }
+
+                /* for (int i = 0; i < frame_size; i++)
                     {
                         const double diff = std::abs(prev_solution[i] - solution[i]);
 
@@ -79,31 +111,28 @@ namespace xod
                             skip_current_frame = true;
                             break;
                         }
-                    }
-                }
+                    } */
 
                 if (skip_current_frame)
                 {
                     break;
                 }
 
-                prev_solution = solution;
-
                 page_start_index++;
                 page_end_index++;
             }
 
-            if (!skip_current_frame)
-            {
-                // solution was right
-                std::cout << "solution = [";
-                for (int i = 0; i < row_count; ++i)
-                {
-                    std::cout << prev_solution[i] << ((i + 1) % row_count ? "," : "");
-                }
-                std::cout << "]" << std::endl;
-                break;
-            }
+            // if (!skip_current_frame)
+            // {
+            //     // solution was right
+            //     std::cout << "solution = [";
+            //     for (int i = 0; i < row_count; ++i)
+            //     {
+            //         std::cout << prev_solution[i] << ((i + 1) % row_count ? "," : "");
+            //     }
+            //     std::cout << "]" << std::endl;
+            //     break;
+            // }
 
             /* for (int i = page_start_index; i < page_end_index; ++i)
             {
--- main.cpp
+++ main.cpp
@@ -3,9 +3,46 @@
 #include "linear_algebra/linear_algebra.h"
 #include <cmath>
 
+void print_combinations(int n, int r)
+{
+    int i = 0;
+    int combinations[r];
+
+    for (i = 0; i < r; ++i)
+        combinations[i] = i;
+
+    //start from the last item
+    i = r - 1;
+    while (combinations[0] < n - r + 1)
+    {
+        // max value an element at i can have is (n - r + i)
+        // so as long as the item at i is at its max value, move backwards i.e. decrement i
+        while (i > 0 && combinations[i] == n - r + i)
+            --i;
+
+        // we have a combination ready
+        std::cout << "[";
+        for (int x : combinations)
+            std::cout << x << ", ";
+        std::cout << "]" << std::endl;
+
+        // increment the current i
+        combinations[i]++;
+
+        // if its not the last item
+        if (i < r - 1)
+        {
+            // the next item should be this item + 1
+            combinations[i + 1] = combinations[i] + 1;
+            i++; //move to next item
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
-    xod::guess_function({1, 2, 3, 4}, {5, 10, 17, 26});
+    // print_combinations(5, 2);
+    xod::guess_function({1, 2, 3, 4, 5}, {3, 14, 38, 82, 152});
 
     return 0;
 }
