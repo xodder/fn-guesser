08e81eb9c6881d7d4126c4229a0a7ae814890913
Stephen Odebiyi <odebiyistephen@gmail.com>
2020-07-15 19:13:56 +0100

    implementing function_guesser with all possible combinations of input

--- function_guesser/function_guesser.cpp
+++ function_guesser/function_guesser.cpp
@@ -7,142 +7,129 @@ namespace xod
 {
     std::string guess_function(std::vector<int> inputs, std::vector<int> outputs)
     {
-        const int input_count = inputs.size();
+        const int input_size = inputs.size();
         const double max_error = 1e-10;
+        double smallest_error = INT_MAX;
+        std::vector<double> best_coefficients;
 
-        for (int frame_size = 1; frame_size <= input_count; frame_size++)
+        for (int sample_size = 2; sample_size <= input_size; sample_size++)
         {
-            int row_count = frame_size;
-            int col_count = frame_size + 1;
+            const int row_count = sample_size;
+            const int col_count = sample_size + 1;
             double matrix[row_count * col_count];
+            double sample_indices[sample_size]; // combinations of inputs to use
+            const int max_sample_index = input_size - sample_size + 1;
+            int pos = 0;
 
-            // (4, 2) -> [0,1], [0,2], [0,3], [1, 2], [1, 3], [2, 3]
-            // for (int i = 0; i < input_count; ++i)
-            // {
-            // }
+            // using combinations of all possible indices of sample_size
+            // build first page indices for current_frame
+            for (pos = 0; pos < sample_size; ++pos)
+                sample_indices[pos] = pos;
 
-            // frame sliding
-            // frame_size = 2
-            // | x x x       x x x       x x x
-            // | x x x  -> | x x x  ->   x x x
-            //   x x x     | x x x     | x x x
-            //   x x x       x x x     | x x x
-            //  p_s_i = 0     = 1        = 2
-            //  p_s_i = page_start_index
-
-            int page_start_index = 0;
-            int page_end_index = page_start_index + frame_size;
-            bool skip_current_frame = false;
-            double smallest_error;
-            std::vector<double> best_solution;
-            bool is_first_test = true;
-
-            // frame sliding implementation
-            while (!skip_current_frame && page_end_index < input_count)
+            pos = sample_size - 1; // start from last index
+            // tries every combination of [sample_size] inputs
+            while (sample_indices[0] < max_sample_index)
             {
-                // build simultaneous equations for page
-                for (int i = page_start_index; i < page_end_index; ++i)
-                {
-                    // transform i into 0...n range
-                    const int zeroed_i = i - page_start_index;
-                    const int offset = zeroed_i * col_count;
+                // max_sample_index_at_pos = input_size - sample_size + pos;
+                while (pos > 0 && sample_indices[pos] == (input_size - sample_size + pos))
+                    --pos; //go backwards
 
-                    // build augmented matrix for input and output set at i
-                    for (int j = 0; j < frame_size; ++j)
+                // std::cout << "indices=[";
+                // for (int x : sample_indices)
+                //     std::cout << x << ", ";
+                // std::cout << "]" << std::endl;
+
+                for (int i = 0; i < sample_size; i++)
+                {
+                    const int sample_index = sample_indices[i];
+                    const int offset = i * col_count;
+
+                    // build augmented matrix for input and output at sample_index
+                    for (int j = 0; j < sample_size; ++j)
                     {
-                        matrix[offset + j] = std::pow(inputs[i], frame_size - j - 1);
+                        // (sample_size - j - 1) inverts the series such that
+                        // 0,1,...,n-1 becomes n-1,n-2,...,1,0
+                        const int exponent = sample_size - j - 1;
+                        matrix[offset + j] = std::pow(inputs[sample_index], exponent);
                     }
-                    matrix[offset + frame_size] = outputs[i]; // augment with output
+                    matrix[offset + sample_size] = outputs[sample_index]; // augment matrix with output
                 }
 
-                auto solution = xod::guass_jordan_elimination(matrix, row_count, col_count);
+                // contains the coefficients of x; where x[0] = coeff. of x^sample_size-1
+                auto coefficients = xod::guass_jordan_elimination(matrix, row_count, col_count);
 
-                // compare the output of the function from the current solution
+                // eliminate extremely small coefficients
+                for (int j = 0; j < row_count; ++j)
+                {
+                    if (coefficients[j] < max_error)
+                        coefficients[j] = 0;
+                }
+
+                // test current coefficients by
+                // comparing the output of the function using the current coefficients
                 // to the actual output given for each input
-                double biggest_error = 0;
-
-                for (int i = 0; i < input_count; ++i)
+                for (int i = 0; i < input_size; ++i)
                 {
                     const double input = inputs[i];
                     const double actual_output = outputs[i];
-                    double output_from_solution = 0;
+                    double computed_output = 0;
 
-                    for (int j = 0; j < row_count; ++j)
+                    std::cout << "input << " << input << std::endl;
+
+                    std::cout << "coefficients = [";
+                    for (int i = 0; i < row_count; ++i)
                     {
-                        output_from_solution += (solution[i] * std::pow(input, j)); // a_i * x^i
+                        std::cout << coefficients[i] << ((i + 1) % row_count ? "," : "");
+                    }
+                    std::cout << "]" << std::endl;
+
+                    // we're starting from the last item because given
+                    // ax^n-1 + bx^n-2 +...+ h = y
+                    // coefficients = [a, b, ..., h] // coefficients in the function
+                    // so that the powers can start from 0, we'll be offsetting j by the sample_size
+                    for (int j = 0; j < sample_size; ++j)
+                    {
+                        // exponent = (sample_size - j - 1) inverts the series such that
+                        // 0,1,...,n-2,n-1 becomes n-1,n-2,...,1,0
+                        const int exponent = sample_size - j - 1;
+                        computed_output += (coefficients[j] * std::pow(input, exponent)); // a_j * x^i
                     }
 
-                    double error = std::abs(output_from_solution - actual_output);
+                    std::cout << computed_output << ", " << actual_output << std::endl;
 
-                    if (error > biggest_error)
+                    double error = std::abs(computed_output - actual_output);
+
+                    if (error < smallest_error)
                     {
-                        biggest_error = error;
+                        smallest_error = error;
+                        best_coefficients = coefficients;
                     }
+                    std::cout << "error = " << error << std::endl;
+                    std::cout << "---------------------" << std::endl;
                 }
 
-                if (biggest_error < smallest_error || is_first_test)
+                sample_indices[pos]++;
+
+                // if its not the last page index
+                if (pos < sample_size - 1)
                 {
-                    best_solution = solution;
-                    smallest_error = biggest_error;
+                    // the next page index should be this current_sample_index + 1
+                    sample_indices[pos + 1] = sample_indices[pos] + 1;
+                    ++pos; //move forwards
                 }
-
-                is_first_test = false;
-
-                // std::cout << page_start_index << ", " << page_end_index << std::endl;
-
-                std::cout << "solution = [";
-                for (int i = 0; i < row_count; ++i)
-                {
-                    std::cout << best_solution[i] << ((i + 1) % row_count ? "," : "");
-                }
-                std::cout << "]" << std::endl;
-
-                if (page_end_index == input_count - 1 && smallest_error < max_error)
-                {
-                    return "";
-                }
-
-                /* for (int i = 0; i < frame_size; i++)
-                    {
-                        const double diff = std::abs(prev_solution[i] - solution[i]);
-
-                        if (diff > precision)
-                        {
-                            skip_current_frame = true;
-                            break;
-                        }
-                    } */
-
-                if (skip_current_frame)
-                {
-                    break;
-                }
-
-                page_start_index++;
-                page_end_index++;
             }
 
-            // if (!skip_current_frame)
-            // {
-            //     // solution was right
-            //     std::cout << "solution = [";
-            //     for (int i = 0; i < row_count; ++i)
-            //     {
-            //         std::cout << prev_solution[i] << ((i + 1) % row_count ? "," : "");
-            //     }
-            //     std::cout << "]" << std::endl;
-            //     break;
-            // }
-
-            /* for (int i = page_start_index; i < page_end_index; ++i)
+            if (smallest_error < max_error)
             {
-                std::cout << "[";
-                for (int j = 0; j < col_count; ++j)
+                std::cout << "best = [";
+                for (int i = 0; i < row_count; ++i)
                 {
-                    std::cout << matrix[i * col_count + j] << ((j + 1) % col_count ? "," : "");
+                    std::cout << best_coefficients[i] << ((i + 1) % row_count ? "," : "");
                 }
                 std::cout << "]" << std::endl;
-            } */
+                std::cout << "done!" << std::endl;
+                break;
+            }
         }
 
         return "";
